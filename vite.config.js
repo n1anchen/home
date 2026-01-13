5660de0e31544b8c6e327eaa844f086ef0c8d0a5
Nianchen <n1anchen@outlook.com>
2026-01-13 14:24:32 +0800

    关闭gz压缩，由边缘网络处理

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
-import viteCompression from "vite-plugin-compression";
+// import viteCompression from "vite-plugin-compression";
 import viteFontCompress from "./scripts/vite-plugin-font-compress.js";
 
 // https://vitejs.dev/config/
@@ -91,7 +91,7 @@ export default ({ mode }) =>
           ],
         },
       }),
-      viteCompression(),
+      // viteCompression(),
     ],
     server: {
       port: "3000",
