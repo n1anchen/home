871a0a06bdb93d0949f149ff2286554fc0ec38db
Nianchen <n1anchen@outlook.com>
2026-01-14 10:25:45 +0800

    修改自定义文本

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,14 +1,35 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+<p>花车颠呀颠  纳西妲睁开眼</p>
+<p>她说  纳西妲的生日就是今天</p>
+<p>花之骑士和侍从们将她发现</p>
+<p>神明啊  可算找到你了  大家都期待与你见面</p>
+<p>花神诞祭开幕了</p>
+<p>人们围着她快乐的转着圈</p>
+<p>花之骑士为她准备了惊喜</p>
+<p>甜蜜的糖果像烟花一般绽放在她身边</p>
+<p>花车碰上了缤纷的鸟雀</p>
+<p>七彩的喜悦飞旋于人们眼前</p>
+</div>
 </template>
 
 <style lang="scss" scoped>
 .more-content {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   margin-top: 20px;
   width: 100%;
   height: 100%;
+  text-align: center;
+
+  p {
+    margin: 3px 0;
+    line-height: 1.8;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+    color: inherit;
+  }
 }
 </style>
