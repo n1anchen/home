c561b727f35ff25fb06b7ed9d81c526387949316
Nianchen <n1anchen@outlook.com>
2026-01-23 09:03:09 +0800

    添加工具站

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, PencilAlt, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Code } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, PencilAlt, CompactDisc, Cloud, Tools, Book, Fire, LaptopCode, Code } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -66,7 +66,7 @@ const siteIcon = {
   PencilAlt,
   Cloud,
   CompactDisc,
-  Compass,
+  Tools,
   Book,
   Fire,
   LaptopCode,
